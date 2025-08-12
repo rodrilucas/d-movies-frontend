0be59acdfc6d65574e7ede2c6932285c221fa86b
rodrilucas <lucas.celery@gmail.com>
2025-08-12 14:14:54 -0300

    feat: create a reusable range input

---
+++ src/components/client/filter/range/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+import styles from "./range.module.scss";
+
+type RangeProps = {
+  label: {
+    htmlFor: string;
+    name: string;
+  };
+  input: {
+    id: string;
+    name: string;
+    min: string;
+    max: string;
+    step: string;
+    defaultValue?: string;
+    value?: number;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  };
+};
+
+export function Range({ label, input }: RangeProps) {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(parseInt(e.target.value));
+  };
+
+  return (
+    <fieldset className={styles.range}>
+      <label htmlFor={label.htmlFor}>
+        {label.name}: <span>{value}</span>
+      </label>
+      <input type="range" {...input} onChange={handleChange} />
+    </fieldset>
+  );
+}
