c3a7e0f68f6fde860fc65404a44903a431539c0b
rodrilucas <lucas.celery@gmail.com>
2025-08-13 08:36:35 -0300

    feat: create a schema for environment variables

---
+++ src/env/index.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import z from "zod";
+
+const envSchema = z.object({
+  BASE_URL: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("Variáveis de ambiente inválidas", z.treeifyError(_env.error));
+  throw new Error("Variáveis de ambiente inválidas");
+}
+
+export const env = _env.data;
